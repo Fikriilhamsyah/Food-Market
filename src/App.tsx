e6bb5b5606fcf3b8d680739e645246d53adb4eae
Fikriilhamsyah <fikriilhamsyah27@gmail.com>
2023-11-12 14:13:11 +0700

    React Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+// import 'react-native-gesture-handler';
 import React from 'react';
 import { SplashScreen } from './pages';
+import { NavigationContainer } from '@react-navigation/native';
 
 const App = () => {
   return (
-    <SplashScreen />
+    <NavigationContainer>
+      <SplashScreen />
+    </NavigationContainer>
   );
 }
 
