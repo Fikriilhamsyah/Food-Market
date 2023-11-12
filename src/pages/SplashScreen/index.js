6aaa1ff9f83299eeaca9d96a98dbcfa61d518df1
Fikriilhamsyah <fikriilhamsyah27@gmail.com>
2023-11-12 13:40:44 +0700

    Custom Fonts

--- src/pages/SplashScreen/index.js
+++ src/pages/SplashScreen/index.js
@@ -4,10 +4,16 @@ import { Logo } from "../../assets";
 
 const SplashScreen = () => {
     return (
-        <View style={{backgroundColor: '#FFC700', flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <View
+            style={{
+                backgroundColor: '#FFC700',
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}>
             <Logo />
             <View style={{height: 38}} />
-            <Text style={{fontSize: 32, color: '#020202'}}>FoodMarket</Text>
+            <Text style={{fontSize: 32, color: '#020202', fontFamily: 'Poppins Reguler'}}>FoodMarket</Text>
         </View>
     );
 }
