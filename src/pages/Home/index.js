88fe86b6d31d2020dedbd27d451cfd59d56c0efe
Fikriilhamsyah <fikriilhamsyah27@gmail.com>
2023-11-17 10:17:47 +0700

    Slicing Home Page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,43 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
+import { ProfileDummy } from '../../assets'
 
 const Home = () => {
   return (
     <View>
-      <Text>Home</Text>
+      <View style={styles.profileContainer}>
+        <View>
+          <Text style={styles.appName}>FoodMarket</Text>
+          <Text style={styles.desc}>Let's get some food</Text>
+        </View>
+        <Image source={ProfileDummy} styles={styles.profile} />
+      </View>
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  profileContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 24,
+    paddingTop: 32,
+    paddingBottom: 24,
+    backgroundColor: 'white',
+  },
+  appName: {
+    fontSize: 22,
+    color: '#020202',
+  },
+  desc: {
+    fontSize: 14,
+    color: '#8D92A3',
+  },
+  profile: {
+    width: 50,
+    height: 50,
+    borderRadius: 8,
+  }
+})
